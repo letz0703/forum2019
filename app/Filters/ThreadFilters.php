eec83fb69d44898d06015c5e5338599d35ca08bb
letz0703 <letz0703@gmail.com>
2019-05-13 01:14:39 +0900

    Refacotry: ThreadFilters@apply

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -5,6 +5,10 @@ namespace App\Filters;
 use App\User;
 use Illuminate\Http\Request;
 
+/**
+ * Class ThreadFilters
+ * @package App\Filters
+ */
 class ThreadFilters
 {
     
@@ -12,22 +16,35 @@ class ThreadFilters
      * @var Request
      */
     protected $request;
+    protected $builder;
     
     public function __construct(Request $request)
     {
-    
         $this->request = $request;
     }
     
     public function apply($builder)
     {
-        //if (!$username = request('by')){
-        if (!$username = $this->request->by){
-            return $builder;
+        $this->builder = $builder;
+        
+        if ($this->request->has('by')){
+            return $this->by($this->request->by);
         }
         
+        return $builder;
+    }
+    
+    /**
+     * @param $username
+     *
+     * @return mixed
+     */
+    
+    protected function by($username)
+    {
         $user = User::where('name', $username)->firstOrFail();
-        return $builder->where('user_id', $user->id);
+        
+        return $this->builder->where('user_id', $user->id);
     }
     
 }
