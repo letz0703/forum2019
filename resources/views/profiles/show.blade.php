b45a2eb699b7f7c68eeb661478d218034b83671f
letz0703 <letz0703@gmail.com>
2019-05-27 03:09:26 +0900

    modify profiles.show.b

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $profileUser->name }}
+
+    <div class="container">
+        <div class="page-header">
+            <h1> {{ $profileUser->name }}
+                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+            </h1>
+        </div>
+    </div>
+
 @endsection
