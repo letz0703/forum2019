6afa57a965417fdd6f69b678de8440dc01074af7
letz0703 <letz0703@gmail.com>
2019-06-13 01:08:44 +0900

    fix route profile

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -64,7 +64,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="route('profile',Auth()->user()) }}">My Profile</a>
+                            <a class="dropdown-item" href="{{route('profile',auth()->user()) }}">My Profile</a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
