26359bb3666b455a1206c0d51456234cc97439f3
letz0703 <letz0703@gmail.com>
2019-06-15 05:36:54 +0900

    session value : ThreadController@store : with(flash,message)

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -5,19 +5,15 @@ namespace App\Http\Controllers;
 use App\Channel;
 use App\Filters\ThreadFilters;
 use App\Thread;
-use App\User;
 use Illuminate\Http\Request;
 
-class ThreadController extends Controller
-{
+class ThreadController extends Controller{
     
-    public function __construct()
-    {
+    public function __construct(){
         $this->middleware('auth')->only(['store', 'create', 'destroy']);
     }
     
-    public function index(Channel $channel, ThreadFilters $filters)
-    {
+    public function index(Channel $channel, ThreadFilters $filters){
         
         $threads = $this->getThreads($channel, $filters);
         
@@ -29,8 +25,7 @@ class ThreadController extends Controller
         return view('threads.index', compact('threads'));
     }
     
-    public function show($channel, Thread $thread)
-    {
+    public function show($channel, Thread $thread){
         //return $thread->load('replies');
         //return Thread::withCount('replies')->first();
         //return $thread;
@@ -41,8 +36,7 @@ class ThreadController extends Controller
         ]);
     }
     
-    public function destroy($channel, Thread $thread)
-    {
+    public function destroy($channel, Thread $thread){
         $this->authorize('update', $thread);
         //if ($thread->user_id != auth()->id()){
         //    abort (403,' Your Do not Have Permission');
@@ -64,8 +58,7 @@ class ThreadController extends Controller
     }
     
     
-    public function store(Request $request)
-    {
+    public function store(Request $request){
         //dd(request()->all());
         $this->validate($request, [
             'title'      => 'required',
@@ -80,11 +73,11 @@ class ThreadController extends Controller
             'body'       => request('body'),
         ]);
         
-        return redirect($thread->path());
+        return redirect($thread->path())
+            ->with('flash', "Your thread has been published" );
     }
     
-    public function create()
-    {
+    public function create(){
         return view('threads.create');
     }
     
@@ -94,8 +87,7 @@ class ThreadController extends Controller
      *
      * @return mixed
      */
-    protected function getThreads(Channel $channel, ThreadFilters $filters)
-    {
+    protected function getThreads(Channel $channel, ThreadFilters $filters){
         $threads = Thread::filter($filters)->latest();
         
         if ($channel->exists){
