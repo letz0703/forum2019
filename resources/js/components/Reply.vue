a149435d88d6a0fcffb220ac3bab688d5c7c04bb
letz0703 <letz0703@gmail.com>
2019-06-24 03:15:06 +0900

    reply.blade 인라인템플릿을 Reply.vue로 이동

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -1,3 +1,38 @@
+<template>
+        <div :id="'reply-'+id" class="card">
+            <div class="card-header">
+                <div class="level">
+                    <h5 class="flex">
+                        <a :href="'/profiles/'+data.owner.name" v-text="data.owner.name"></a>
+                        said {{ data.created_at }}
+                    </h5>
+                    <!--@if ( auth()->check() )-->
+                    <!--<div>-->
+                        <!--<favorite :reply = " {{ $reply }}"></favorite>-->
+                    <!--</div>-->
+                    <!--@endif-->
+                </div>
+            </div>
+            <div class="card-body">
+                <div v-if="editing">
+                    <div class="form-group">
+                        <textarea class="form-control" v-model="body"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-sm" @click="update">update</button>
+                    <button class="btn btn-link btn-sm" @click = "cancel">cancel</button>
+                </div>
+                <div v-else v-text="body"></div>
+            </div>
+            <!--@can('update')-->
+            <!--<div class="card-footer level">-->
+                <!--<button class="btn btn-outline-dark btn-sm mr-1" @click="editing = true">-->
+                    <!--Edit-->
+                <!--</button>-->
+                <!--<button class="btn btn-danger btn-sm" @click="destroy">delete</button>-->
+            <!--</div>-->
+            <!--@endcan-->
+        </div>
+</template>
 <script>
     import Favorite from './Favorite.vue';
 
@@ -10,7 +45,8 @@
             return {
                 editing: false,
                 body: this.data.body,
-                tempbody: ''
+                tempbody: '',
+                id: this.data.id
             };
         },
 
