8b484d0fee0ed7190d6907e9de1334716623ac97
letz0703 <letz0703@gmail.com>
2019-06-23 19:30:17 +0900

    delete associate Favorite Activitiy when deleting reply

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -5,6 +5,12 @@ namespace App;
 
 trait Favoritable
 {
+    protected static function bootFavoritable()
+    {
+        static::deleting(function($model){
+            $model->favorites->each->delete();
+        });
+    }
     
     public function favorites()
     {
