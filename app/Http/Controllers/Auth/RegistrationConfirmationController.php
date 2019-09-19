68aff9d6d359982cbd1ea37f4f6be2b65f75156b
Chang Man Kim <letz0703@gmail.com>
2019-09-19 21:44:55 +0900

    Apply fixes from StyleCI (#11)

--- app/Http/Controllers/Auth/RegistrationConfirmationController.php
+++ app/Http/Controllers/Auth/RegistrationConfirmationController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers\Auth;
 use App\User;
 use App\Http\Controllers\Controller;
 
-class RegistrationConfirmationController extends Controller{
+class RegistrationConfirmationController extends Controller
+{
     public function index()
     {
         $user = User::where('confirmation_token', request('token'))->first();
-        
-        if ( ! $user){
+
+        if (! $user) {
             return redirect(route('threads'))
                 ->with('flash', 'Unknown token');
         }
-        
+
         $user->confirm();
-        
+
         return redirect(route('threads'))
             ->with('flash', 'Your email is confirmed. Your can post from now on.');
     }
