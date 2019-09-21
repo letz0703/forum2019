9d8ec780a66f4109938704547fd94480a749859c
Chang Man Kim <letz0703@gmail.com>
2019-09-22 00:42:28 +0900

    Apply fixes from StyleCI (#20)

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -2,53 +2,54 @@
 
 namespace App;
 
-trait Favoritable{
+trait Favoritable
+{
     protected static function bootFavoritable()
     {
-        static::deleting(function ($model){
+        static::deleting(function ($model) {
             $model->favorites->each->delete();
         });
     }
-    
+
     public function favorites()
     {
         return $this->morphMany(Favorite::class, 'favorited');
     }
-    
+
     public function favorite()
     {
         $attributes = ['user_id' => auth()->id()];
-        
-        if ( ! $this->favorites()->where($attributes)->exists()){
+
+        if (! $this->favorites()->where($attributes)->exists()) {
             Reputation::award(auth()->user(), Reputation::REPLY_IS_FAVORITED);
-            
+
             return $this->favorites()->create($attributes);
         }
     }
-    
+
     public function unfavorite()
     {
         $attributes = ['user_id' => auth()->id()];
         Reputation::swipe(auth()->user(), Reputation::REPLY_IS_FAVORITED);
-        
+
         $this->favorites()->get()->each->delete();
         //$this->favorites()->where($attributes)->get()->each(function($favorite){
         //    $favorite->delete();
         //});
         //$this->favorites()->where($attributes)->delete();
     }
-    
+
     public function isFavorited()
     {
         //return $this->favorites()->where(['user_id' => auth()->id()])->exists();
         return (bool) $this->favorites->where('user_id', auth()->id())->count();
     }
-    
+
     public function getIsFavoritedAttribute()
     {
         return $this->isFavorited();
     }
-    
+
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
--- app/Http/Controllers/Admin/ChannelController.php
+++ app/Http/Controllers/Admin/ChannelController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Channel;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class ChannelController extends Controller
 {
@@ -16,60 +15,56 @@ class ChannelController extends Controller
         return view('admin.channels.index')
             ->with('channels', Channel::with('threads')->get());
     }
-    
+
     public function create()
     {
         return view('admin.channels.create');
     }
-    
+
     public function store()
     {
         $data = request()->validate([
             'name'        => 'required|unique:channels',
             'description' => 'required',
         ]);
-        
+
         $channel = Channel::create($data + ['slug' => str_slug($data['name'])]);
-        
+
         Cache::forget('channels');
-        
-        if (request()->wantsJson()){
+
+        if (request()->wantsJson()) {
             return response($channel, 201);
         }
-        
+
         return redirect(route('admin.channels.index'))
             ->with('flash', 'Your channel has been created');
     }
-    
+
     public function edit(Channel $channel)
     {
         return view('admin.channels.edit', compact('channel'));
     }
-    
-    
+
     public function update(Request $request, Channel $channel)
     {
         $this->validate($request, [
             'name'        => 'required|spamfree|unique:channels',
             'description' => 'required|spamfree',
         ]);
-        
+
         $channel->update([
             'name'        => request('name'),
             'slug'        => $channel->make_slug(request('name')),
             'description' => request('description'),
         ]);
-        
+
         cache()->forget('channels');
-        
-        if (request()->wantsJson()){
+
+        if (request()->wantsJson()) {
             return response($channel, 200);
         }
-        
+
         return redirect(route('admin.channels.index'))
             ->with('flash', 'Your channel has been updated');
-        
     }
-    
-    
 }
