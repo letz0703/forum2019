6cd283b3b2eadce1b254d283c8171ea4bb65d4aa
Chang Man Kim <letz0703@gmail.com>
2019-09-24 15:20:15 +0000

    Apply fixes from StyleCI

--- app/Channel.php
+++ app/Channel.php
@@ -10,43 +10,41 @@ class Channel extends Model
     protected $casts = [
         'archived' => 'boolean',
     ];
-    
+
     protected static function boot()
     {
         parent::boot();
-        
-        static::addGlobalScope('active', function ($builder){
+
+        static::addGlobalScope('active', function ($builder) {
             $builder->where('archived', false)
                     ->orderBy('name', 'asc');
         });
     }
-    
+
     public function getRouteKeyName()
     {
         return 'slug';
     }
-    
+
     public function threads()
     {
         return $this->hasMany('App\Thread');
     }
-    
-    function make_slug($string)
+
+    public function make_slug($string)
     {
         $slug = preg_replace('/\s+/u', '-', trim($string));
-        
+
         return $slug = str_slug($slug);
     }
-    
+
     public function archive()
     {
-        $this->update(['archived' => true,]);
+        $this->update(['archived' => true]);
     }
-    
+
     protected static function withArchived()
     {
         return (new static)->newQueryWithoutScope('active');
     }
-    
-    
 }
