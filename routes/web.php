e2eda942e7f0795f9131689d0ce0915118629fa9
Chang Man Kim <letz0703@gmail.com>
2019-10-01 15:47:11 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -87,10 +87,10 @@ Route::group([
          ->name('admin.channels.update');
 });
 
-Route::get('/search', function() {
+Route::get('/search', function () {
     $query = '*'; // <-- Change the query for testing.
-    
+
     $result = App\Thread::search($query)->get();
-    
+
     return $result;
 });
