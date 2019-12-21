242a7b3923f3a1af3e3f230ecac13e11742f6060
Chang Man Kim <letz0703@gmail.com>
2019-12-21 18:23:07 +0000

    Apply fixes from StyleCI

--- app/Http/Requests/CreatePostRequest.php
+++ app/Http/Requests/CreatePostRequest.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Requests;
 
-use App\Reply;
-use Illuminate\Support\Facades\Gate;
 use App\Exceptions\ThrottleException;
+use App\Reply;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class CreatePostRequest extends FormRequest
 {
